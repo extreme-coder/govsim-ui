23087bd9907b6e20f8dce4e2bd2d1199a69c9f06
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-02 19:09:17 -0700

    Strapi v4 update

--- src/services/govsim.js
+++ src/services/govsim.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const govsimApi = createApi({
   reducerPath: 'govsimApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1337/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1337/api/' }),
   endpoints: (builder) => ({
     getEntity: builder.query({
       query: (arg) => {
