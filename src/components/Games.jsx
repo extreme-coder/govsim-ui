6dda41e1bacc5c2c2b17036fb81b242e6d3c3588
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-03 12:12:26 -0700

    Join game using private join code

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { useGetEntitiesQuery, useAddEntityMutation } from '../services/govsim';
-import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import TextField from './common/TextField';
+import { Form, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+import { useNavigate } from 'react-router-dom'
+import * as Yup from 'yup';
 
 export default function Games() {
+  const JoinSchema = Yup.object().shape({
+    join_code: Yup.string()
+      .required('Required'),  
+  });
+
   const { data, error, isLoading } = useGetEntitiesQuery('country')
   const [ addEntity, { isLoading: isUpdating } ] = useAddEntityMutation()
-  const joinGame = (id) => {    
-    addEntity({name:'party', body:{data:{country:id, name:'hello'}}})
+  const navigate = useNavigate()
+  const joinGame = (values) => {    
+    navigate(`/joingame/${values.join_code}`)
   }
   return (
     <div className="container">
@@ -19,6 +29,22 @@ export default function Games() {
               </div>
           )}
         </div>
+        <div>
+        Join a Private Game with code : 
+        <Formik enableReinitialize validationSchema={JoinSchema} onSubmit={joinGame} initialValues={{ join_code: ''}}>
+        {(props) => (
+          <Form noValidate onSubmit={props.handleSubmit}>
+            <Form.Group controlId="formBasicName">
+              <TextField name="join_code" label="Join Code" placeholder="six letter join code" />                
+              <Button variant="primary" type="submit">
+                Join Game
+              </Button>
+            </Form.Group>
+          </Form>
+        )}
+        </Formik>           
+        </div>
+        <Link to="/newgame" ><Button>Create New Game</Button></Link>
       </div>
     </div>
   );
