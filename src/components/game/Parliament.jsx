4301ba2c3202cbf73cc8e8651c82a4d01004dc94
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-20 19:39:57 -0700

    before first election, hide parliament

--- src/components/game/Parliament.jsx
+++ src/components/game/Parliament.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useGetEntitiesByFieldQuery} from '../../services/govsim';
+import {useGetEntitiesByFieldQuery, useGetEntityQuery} from '../../services/govsim';
 import { VictoryLabel} from 'victory';
 import PieChart from '../common/PieChart';
 
@@ -7,6 +7,8 @@ import PieChart from '../common/PieChart';
 export default function Parliament(props) {
   const { countryId } = props
   const { data: parties } = useGetEntitiesByFieldQuery({ name: 'party', field: 'country', value: countryId, relation: 'id', populate: true })
+  console.log(countryId)
+  const { data: country } = useGetEntityQuery({ name: 'country', id: countryId })
   const getData = (parties) => {
     let data = {}
     let totalSeats = 0
@@ -19,11 +21,14 @@ export default function Parliament(props) {
   }
   return (    
     <div className="container" style={{maxWidth:"500px"}}>
-      {parties && 
+      {parties && country && (country.election_called) &&
       <PieChart 
         data={getData(parties)} 
         innerRadius={40}
       />
+      }
+      {country &&
+        <div>{!(country.election_called) && <p>test</p>}</div>
       }
     </div>
   );
