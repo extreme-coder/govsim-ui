50c1107c11a55a179a22e4ad5320d7f0e63c6e80
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-25 14:29:01 -0700

    bigger column for votes and bills

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -24,6 +24,38 @@ export default function Game() {
 
   return (
     <>
+
+    <div className="container-fluid pt-2">
+      <div className="row">
+        <div className="col-xl-6 col-lg-6">
+              <div className="card shadow-sm">
+                <div className="card-body">
+                  {country && party &&
+                    <Platform countryId={country.data[0].id} partyId={party.data[0].id} isPartyReady={party.data[0].attributes.ready_for_election} electionsOccurred={country.data[0].attributes.elections_occurred} />
+                  }
+                </div>
+              </div>
+        </div>
+        <div className="col-xl-6 col-lg-6">
+
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xl-6 col-lg-6">
+        <div className="card shadow-sm">
+                <div className="card-body">
+                  {country && party &&
+                    <VotesInSession countryId={country.data[0].id} partyId={party.data[0].id} />
+                  }
+                </div>
+              </div>
+        </div>
+        <div className="col-xl-6 col-lg-6">
+
+        </div>
+      </div>
+    </div>
+
       <div className="game pt-5" >
         <div className="container">
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
@@ -44,24 +76,7 @@ export default function Game() {
                 </div>
               </div>
             </div>
-            <div className="col">
-              <div className="card shadow-sm">
-                <div className="card-body">
-                  {country && party &&
-                    <Platform countryId={country.data[0].id} partyId={party.data[0].id} isPartyReady={party.data[0].attributes.ready_for_election} electionsOccurred={country.data[0].attributes.elections_occurred} />
-                  }
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card shadow-sm">
-                <div className="card-body">
-                  {country && party &&
-                    <VotesInSession countryId={country.data[0].id} partyId={party.data[0].id} />
-                  }
-                </div>
-              </div>
-            </div>
+                       
             <div className="col">
               <div className="card shadow-sm">
                 <div className="card-body">
