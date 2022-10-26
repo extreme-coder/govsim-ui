72a2c2195a9739f58eefe6388db02becee7bb814
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-26 14:20:58 -0700

    Use Apex Charts

--- src/components/game/Demoraphics.jsx
+++ src/components/game/Demoraphics.jsx
@@ -1,21 +1,61 @@
 import React from 'react';
-import {useGetBlockGroupsQuery} from '../../services/govsim';
-import { VictoryLabel} from 'victory';
-import PieChart from '../common/PieChart';
-
+import { useGetBlockGroupsQuery } from '../../services/govsim';
+import Chart from 'react-apexcharts';
 
 export default function Demographics(props) {
   const { countryId } = props
   const { data: blocks } = useGetBlockGroupsQuery(countryId)
-  
-  return (    
-    <div className="container" style={{maxWidth:"500px"}}>
-      {blocks && 
-      <PieChart 
-        data={blocks} 
-        innerRadius={40}
-      />
+
+  const apexDonutOpts = {
+    chart: {
+      height: 320,
+      type: 'pie',
+    },    
+    colors: ['#727cf5', '#6c757d', '#0acf97', '#fa5c7c', '#e3eaef'],
+    legend: {
+      show: true,
+      position: 'right',
+      horizontalAlign: 'center',
+      verticalAlign: 'middle',
+      floating: false,
+      fontSize: '14px',
+      offsetX: 0,
+      offsetY: -10,
+    },
+    responsive: [
+      {
+        breakpoint: 600,
+        options: {
+          chart: {
+            height: 240,
+          },
+          legend: {
+            show: false,
+          },
+        },
+      },
+    ],
+  };
+
+  if(blocks) {
+    apexDonutOpts.labels = blocks.labels
+  }
+
+  return (
+    <div className="container" style={{ maxWidth: "500px" }}>
+      {blocks &&
+        <Chart
+          options={apexDonutOpts}
+          series={blocks.data}
+          type="donut"
+          height={320}
+          className="apex-charts"
+        />
       }
     </div>
   );
 };
+
+
+
+
--- src/components/game/Parliament.jsx
+++ src/components/game/Parliament.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {useGetEntitiesByFieldQuery, useGetEntityQuery} from '../../services/govsim';
-import { VictoryLabel} from 'victory';
-import PieChart from '../common/PieChart';
+import { useGetEntitiesByFieldQuery, useGetEntityQuery } from '../../services/govsim';
+import Chart from 'react-apexcharts';
+
 
 
 export default function Parliament(props) {
@@ -9,23 +9,57 @@ export default function Parliament(props) {
   const { data: parties } = useGetEntitiesByFieldQuery({ name: 'party', field: 'country', value: countryId, relation: 'id', populate: true })
   console.log(countryId)
   const { data: country } = useGetEntityQuery({ name: 'country', id: countryId })
-  const getData = (parties) => {
-    let data = {}
-    let totalSeats = 0
+  const apexDonutOpts = {
+    chart: {
+      height: 320,
+      type: 'pie',
+    },
+    colors: ['#727cf5', '#6c757d', '#0acf97', '#fa5c7c', '#e3eaef'],
+    labels: [],
+    legend: {
+      show: true,
+      position: 'right',
+      horizontalAlign: 'center',
+      verticalAlign: 'middle',
+      floating: false,
+      fontSize: '14px',
+      offsetX: 0,
+      offsetY: -10,
+    },
+    responsive: [
+      {
+        breakpoint: 600,
+        options: {
+          chart: {
+            height: 240,
+          },
+          legend: {
+            show: false,
+          },
+        },
+      },
+    ],
+  };
+
+  let data = []
+  
+  if (parties) {    
     parties.data.forEach((party) => {
-      data[party.attributes.name] = party.attributes.seats      
-      totalSeats += party.attributes.seats 
+      data.push(party.attributes.seats)
+      apexDonutOpts.labels.push(party.attributes.name)
     })
-    return Object.keys(data).map((k) => ({x: k, y: data[k]*100/totalSeats}))
-    
   }
-  return (    
-    <div className="container" style={{maxWidth:"500px"}}>
+
+  return (
+    <div className="container" style={{ maxWidth: "500px" }}>
       {parties && country && (country.data.attributes.elections_occurred) &&
-      <PieChart 
-        data={getData(parties)} 
-        innerRadius={40}
-      />
+        <Chart
+          options={apexDonutOpts}
+          series={data}
+          type="donut"
+          height={320}
+          className="apex-charts"
+        />
       }
       {country &&
         <div>{!(country.data.attributes.elections_occurred) && <p>No elections have occurred yet</p>}</div>
