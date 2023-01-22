05e55c6a8a2701444d81661d84d7a3efa6fdf0ed
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-01-22 12:04:41 -0800

    Remove option to change game states

--- src/components/game/PlayerInfo.jsx
+++ src/components/game/PlayerInfo.jsx
@@ -38,23 +38,13 @@ export default function PlayerInfo(props) {
           <div className="col-xl-3 col-lg-3 col-md-3">Party Name: {party.attributes.name}</div>
           <div className="col-xl-3 col-lg-3 col-md-3">Remaining Budget: {party.attributes.budget}</div>
           <div className="col-xl-3 col-lg-3 col-md-3" >Score: {party.attributes.points}</div>
-        <div className="col-xl-3 col-lg-3 col-md-3">
-          {country.attributes.status === 'PARLIAMENT' && !isPartyReady && <Button onClick={() => readyForElection()}>Ready for Election</Button>}
-          {country.attributes.status === 'PARLIAMENT' && isPartyReady && <div>
-            <p>You are ready for the next election</p>
-            <Button onClick={() => revertReadyForElection()}>Go Back</Button>
-          </div>}
-          {country.attributes.status === 'CAMPAIGN' && !party.attributes.finished_campaign && <Button onClick={() => finishedCampaign()}>Finish Campaigning</Button>}
-          {country.attributes.status === 'CAMPAIGN' && party.attributes.finished_campaign && <div>
-            <p>You are finished campaigning</p>
-            <Button onClick={() => revertFinishedCampaign()}>Go Back</Button>
-          </div>}
-          {country.attributes.status === 'COALITIONS' && !party.attributes.ready_for_parliament && <Button onClick={() => readyForParliament()}>Ready for Parliament</Button>}
-          {country.attributes.status === 'COALITIONS' && party.attributes.ready_for_parliament && <div>
+          {country.attributes.status === 'COALITIONS' && <div className="col-xl-3 col-lg-3 col-md-3">         
+            {!party.attributes.ready_for_parliament && <Button onClick={() => readyForParliament()}>Ready for Parliament</Button>}
+            {party.attributes.ready_for_parliament && <div>
             <p>You are ready for Parliament Session</p>
             <Button onClick={() => revertReadyForParliament()}>Go Back</Button>
           </div>}
-        </div>
+        </div>}
       </div>
     </div>
   );
