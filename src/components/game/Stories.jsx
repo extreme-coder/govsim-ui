9135d3ede352fc48c924b31017d22d49ed8ecb10
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-02-10 17:54:14 -0800

    newer stories show up first

--- src/components/game/Stories.jsx
+++ src/components/game/Stories.jsx
@@ -2,7 +2,7 @@ import { useGetEntitiesByFieldQuery } from '../../services/govsim';
 
 
 export default function MessageHandler(props) {
-  const { data: stories } = useGetEntitiesByFieldQuery({ name: 'story', field: 'country', relation:'id',  value: props.country.id })
+  const { data: stories } = useGetEntitiesByFieldQuery({ name: 'story', field: 'country', relation:'id',  value: props.country.id, sort: 'desc' })
   return (
     (stories && stories.data.map((story) =>
       {
